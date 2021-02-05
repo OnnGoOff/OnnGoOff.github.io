aca14c569802e656d8adba1e214782c9981db9c6
CaptainBluBlu <64024688+CaptainBluBlu@users.noreply.github.com>
2021-02-06 01:43:27 +0800

    Spotify visualiser project :sparkles:

--- src/dev_profiles/Glenn/index.tsx
+++ src/dev_profiles/Glenn/index.tsx
@@ -3,24 +3,29 @@ import React, { useState } from 'react';
 const Glenn = () => {
   const [counter, setCounter] = useState(0);
 
+
   return (
     <>
-      <h1>Im Your mother woon!</h1>
-      <p>The counter is now at: {counter}</p>
-      <button
-        onClick={() => {
-          setCounter((curr) => curr + 1);
-        }}
-      >
-        Increment
+      <h1>This is my homepage</h1>
+      <div className="SimpleCounter">
+        <h1>Im Your mother woon!</h1>
+        <p>The counter is now at: {counter}</p>
+        <button
+          onClick={() => {
+            setCounter((curr) => curr + 1);
+          }}
+        >
+          Increment
       </button>
-      <button
-        onClick={() => {
-          setCounter((curr) => curr - 1);
-        }}
-      >
-        Decrement
+        <button
+          onClick={() => {
+            setCounter((curr) => curr - 1);
+          }}
+        >
+          Decrement
       </button>
+      </div>
+
     </>
   );
 };
